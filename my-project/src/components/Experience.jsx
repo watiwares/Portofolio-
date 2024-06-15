7f4ea28f17ebbfd48925207a349086200b880b58
AnthonynCordelle <anthonyn.cordelle@hotmail.fr>
2024-06-15 13:15:05 +0200

    first ccommit

---
+++ my-project/src/components/Experience.jsx
@@ -0,0 +1,33 @@
+import { EXPERIENCES } from "../constants"
+
+const Experience = () => {
+  return (
+    <div className="border-b border-neutral-900pb-4">
+        <h2 className="my-20 text-center">Experience</h2>
+    <div>
+        {/* à revoir */}
+        {EXPERIENCES.map((experience,index)=> (
+            <div 
+            key={index} 
+            className="mb-8 flex flex-wrap lg:justify-center"
+            >    
+                <div className="w-full lg:w-1/4">
+                    <p className="mb-2 text-sm text-neutral-400">{experience.year}</p>
+                
+                </div>
+
+                <div className="w-full max-w-xl lg:w-3/4">
+                    <h6 className="mb-2 font-semibold">{experience.role} - {""} 
+                    <span className="text-sm text-purple-100"> {experience.company}</span>
+                    </h6>
+
+                </div>
+            </div>       
+        ))}
+    </div>
+
+    </div>      
+  )
+}
+
+export default Experience
